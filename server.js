4d135238e66c7bc1e7342b3e2eacbc782283653e
jacksoncunha-w1 <jackson.cunha@docente.senai.br>
2026-08-06 09:52:16 -0300

    add package.json

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const app = express()
 
 app.use(express.json())
 
+app.get("/alunos", (req, res) => {
+    console.log("Acessando dados da API")
+})
 
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
+    console.log("Servidor rodando na porta 3000")
 })
